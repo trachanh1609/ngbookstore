bfa0c4037a903915f9268273b57d91af38f72229
Vu Nguyen <trachanh@gmail.com>
2018-12-03 14:08:47 +0200

    add button, routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { BooksComponent } from './books/books.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  { path: '', component: BooksComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
